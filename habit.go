ef1308cff1ebf3f1ca1817a4011747b7a61174bb
Jakub Jarosz <jakub@jarosz.dev>
2022-10-29 20:42:54 +0100

    WIP refactoring

--- habit.go
+++ habit.go
@@ -3,135 +3,138 @@ package habit
 import (
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
-	"io"
-	"io/fs"
-	"io/ioutil"
 	"os"
 	"time"
 )
 
-type Habit struct {
-	Name   string      `json:"name"`
-	Dates  []time.Time `json:"dates"`
-	Output io.Writer   `json:"-"`
+type FileStore struct {
+	Path string
 }
 
-type option func(*Habit) error
-
-func WithOutput(w io.Writer) option {
-	return func(h *Habit) error {
-		if w == nil {
-			return errors.New("nil habit writer")
-		}
-		h.Output = w
-		return nil
-	}
-}
-
-func New(name string, opts ...option) (Habit, error) {
-	if name == "" {
-		return Habit{}, errors.New("nil habit name")
-	}
-	h := Habit{
-		Name:   name,
-		Dates:  []time.Time{habitDate(time.Now())},
-		Output: os.Stdout,
-	}
-	for _, opt := range opts {
-		opt(&h)
-	}
-	fmt.Fprintf(h.Output, "Good luck with your new habit '%s'! Don't forget to do it again tomorrow.\n", h.Name)
-	return h, nil
-}
-
-// Streak returns number of dates logged.
-func (h Habit) Streak() int {
-	return len(h.Dates)
-}
-
-// LogActivity add activity to the habit counter.
-func (h *Habit) LogActivity() {
-	today := habitDate(time.Now())
-	lastRecorded := h.Dates[len(h.Dates)-1]
-	if !today.After(lastRecorded) {
-		return
-	}
-	// Shall we start new streak or continue recording dates to current habit.
-	diff := int(today.Sub(lastRecorded).Hours() / 24)
-	if diff > 1 {
-		// Streak broken, remove dates and start a new streak.
-		h.Dates = []time.Time{today}
-		fmt.Fprintf(h.Output, "You last did the habit '%s' %d days ago, so you're starting a new streak today. Good luck!\n", h.Name, diff)
-		return
-	}
-	h.Dates = append(h.Dates, today)
-	fmt.Fprintf(h.Output, "Nice work: you've done the habit '%s' for %d days in a row now. Keep it up!\n", h.Name, len(h.Dates))
-}
-
-// Check prints number of days in the current streak.
-func (h Habit) Check() int {
-	today := habitDate(time.Now())
-	firstRecorded := h.Dates[0]
-	lastRecorded := h.Dates[len(h.Dates)-1]
-
-	diff := int(today.Sub(lastRecorded).Hours() / 24)
-	if diff > 1 {
-		fmt.Fprintf(h.Output, "It's been %d days since you did '%s'. It's okay, life happens. Get back on that horse today!\n", diff, h.Name)
-		return diff
-	}
-	streak := int(today.Sub(firstRecorded).Hours()/24) + 1
-	fmt.Fprintf(h.Output, "You're currently on a %d-day streak for '%s'. Stick to it!\n", streak, h.Name)
-	return streak
-}
-
-func FromFile(path string, opts ...option) (Habit, error) {
-	if path == "" {
-		return Habit{}, errors.New("empty path to habit file")
-	}
-	h, err := LoadFromFile(path)
-	if err != nil {
-		return Habit{}, err
-	}
-	habit := Habit{
-		Name:   h.Name,
-		Dates:  h.Dates,
-		Output: os.Stdout,
-	}
-	for _, opt := range opts {
-		opt(&habit)
-	}
-	return habit, nil
-}
-
-func SaveToFile(path string, h Habit) error {
+func (f FileStore) Save(h *Habit) error {
 	hb := Habit{
-		Name:  h.Name,
-		Dates: h.Dates,
+		Name:   h.Name,
+		Date:   h.Date,
+		Streak: h.Streak,
 	}
 	d, err := json.Marshal(hb)
 	if err != nil {
 		return err
 	}
-	err = ioutil.WriteFile(path, d, 0644)
+	err = os.WriteFile(f.Path, d, 0644)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func LoadFromFile(path string) (Habit, error) {
+func (f FileStore) Load() (*Habit, error) {
 	var h Habit
-	b, err := ioutil.ReadFile(path)
+	b, err := os.ReadFile(f.Path)
 	if err != nil {
-		return Habit{}, err
+		return nil, err
 	}
 	err = json.Unmarshal(b, &h)
 	if err != nil {
-		return Habit{}, err
+		return nil, err
 	}
-	return h, nil
+	return &h, nil
+}
+
+type Habit struct {
+	Name string
+	// Date it's a date when habit activity was last recorded
+	Date time.Time
+	// Streak represents number of consecutive days
+	// when habit was recorded.
+	Streak int
+}
+
+func New(name string) (*Habit, error) {
+	if name == "" {
+		return nil, errors.New("nil habit name")
+	}
+	h := Habit{
+		Name:   name,
+		Date:   habitDate(time.Now()),
+		Streak: 1,
+	}
+	return &h, nil
+}
+
+func (h *Habit) startNewStreak() {
+	h.Date = habitDate(time.Now())
+	h.Streak = 1
+}
+
+func (h *Habit) continueStreak() {
+	h.Date = habitDate(time.Now())
+	h.Streak += 1
+}
+
+func (h Habit) checkStreak() int {
+	return int(time.Since(h.Date).Hours() / 24)
+}
+
+// Check verifies if the streak is broken or not.
+// Returned value represents number of days since
+// the habit was logged.
+func (h Habit) Check() (int, string) {
+	diff := h.checkStreak()
+	if diff == 0 || diff == 1 {
+		return diff, fmt.Sprintf("You are currently on a %d-day streak for '%s'. Stick to it!\n", h.Streak, h.Name)
+	}
+	return diff, fmt.Sprintf("It's been %d days since you did '%s'. It's ok, life happens. Get back on that horse today!\n", diff, h.Name)
+}
+
+// Log adds activity to an existing habit streak or
+// starts a new streak if the current one was broken.
+// It returns streak lenght and a message.
+func (h *Habit) Log() (int, string) {
+	diff := h.checkStreak()
+	if diff == 0 {
+		return h.Streak, ""
+	}
+	if diff > 1 {
+		h.startNewStreak()
+		return h.Streak, fmt.Sprintf("You last did the habit '%s' %d days ago, so you're starting a new streak today. Good luck!\n", h.Name, diff)
+	}
+	h.continueStreak()
+	return h.Streak, fmt.Sprintf("Nice work: you've done the habit '%s' for %d days in a row now. Keep it up!\n", h.Name, h.Streak)
+}
+
+func SaveToFile(path string, h *Habit) error {
+	hb := Habit{
+		Name:   h.Name,
+		Date:   h.Date,
+		Streak: h.Streak,
+	}
+	d, err := json.Marshal(hb)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(path, d, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadFromFile(path string) (*Habit, error) {
+	if path == "" {
+		return nil, errors.New("missing path")
+	}
+	var h Habit
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(b, &h)
+	if err != nil {
+		return nil, err
+	}
+	return &h, nil
 }
 
 // habitDate truncates time to full days.
@@ -139,52 +142,87 @@ func habitDate(t time.Time) time.Time {
 	return t.UTC().Truncate(24 * time.Hour)
 }
 
+/*
 func RunCLI() {
 	fset := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 	fset.Parse(os.Args[1:])
 
-	args := fset.Args()
+	path, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
 
+	args := fset.Args()
 	// No args, so check habit and the file
 	if len(args) == 0 {
-		h, err := FromFile("./habit.json")
+		h, err := LoadFromFile(storePath)
 		if errors.Is(err, fs.ErrNotExist) {
 			fset.Usage()
 			os.Exit(0)
 		}
-		h.Check()
+
 		os.Exit(0)
 	}
 
 	habitName := args[0]
 
-	h, err := FromFile("./habit.json")
+	h, err := LoadFromFile(storePath)
 	if errors.Is(err, fs.ErrNotExist) {
 		// start new habit
 		h, err := New(habitName)
 		if err != nil {
+			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
-		err = SaveToFile("./habit.json", h)
+		b, err := NewBoard(h, WithOutput(os.Stdout))
 		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(1)
+		}
+		b.LogActivity()
+		err = SaveToFile(storePath, h)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
 		os.Exit(0)
 	}
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
+
 	if habitName != h.Name {
 		// start new habit
 		h, err := New(habitName)
 		if err != nil {
+			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
-		err = SaveToFile("./habit.json", h)
+		b, err := NewBoard(h, WithOutput(os.Stdout))
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		b.LogActivity()
+
+		err = SaveToFile(storePath, h)
 		if err != nil {
 			os.Exit(1)
 		}
 		os.Exit(0)
 	}
 
-	h.LogActivity()
-	SaveToFile("./habit.json", h)
+	b, err := NewBoard(h, WithOutput(os.Stdout))
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	b.LogActivity()
+
+	SaveToFile(storePath, h)
 	os.Exit(0)
 }
+*/
--- habit_test.go
+++ habit_test.go
@@ -1,202 +1,129 @@
 package habit_test
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/qba73/habit"
-	"github.com/rogpeppe/go-internal/testscript"
 )
 
 func TestHabit_StartsNewActivityWithNameAndInitialDate(t *testing.T) {
 	t.Parallel()
-	fakeTerminal := &bytes.Buffer{}
-	habitName := "jog"
-	h, err := habit.New(habitName, habit.WithOutput(fakeTerminal))
+	h, err := habit.New("jog")
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := fmt.Sprintf(messageStartNewHabit, habitName)
-	got := fakeTerminal.String()
+	want := time.Now().UTC().Truncate(24 * time.Hour)
+	got := h.Date
 	if want != got {
-		t.Errorf("want %s, got %s", want, got)
-	}
-
-	wantTime := time.Now().UTC().Truncate(24 * time.Hour)
-	gotTime := h.Dates[0]
-	if wantTime != gotTime {
 		t.Errorf("want %v, got %v", want, got)
 	}
 }
 
 func TestHabit_DoesNotLogDuplicatedActivityOnTheSameDay(t *testing.T) {
 	t.Parallel()
-	h, err := habit.New("jog", habit.WithOutput(io.Discard))
+	h, err := habit.New("jog")
 	if err != nil {
 		t.Fatal(err)
 	}
-	h.LogActivity()
-	want := 1
-	got := len(h.Dates)
+	h.Log()
+	wantDate := time.Now().UTC().Truncate(24 * time.Hour)
+	gotDate := h.Date
+	if wantDate != gotDate {
+		t.Fatalf("want %v, got %v", wantDate, gotDate)
+	}
+
+	wantStreak := 1
+	gotStreak := h.Streak
+	if wantStreak != gotStreak {
+		t.Errorf("want %d, got %d", wantStreak, gotStreak)
+	}
+}
+
+func TestHabit_ChecksStreakLength(t *testing.T) {
+	t.Parallel()
+	h, err := habit.New("jog")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 0
+	got, _ := h.Check()
 	if want != got {
-		t.Fatalf("want %d, got %d", want, got)
+		t.Errorf("want %d, got %d", want, got)
+	}
+
+	h.Date = time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1)
+	want = 1
+	got, _ = h.Check()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
 
 func TestHabit_RecordsActivityOnNextDay(t *testing.T) {
 	t.Parallel()
-	h, err := habit.New("jog", habit.WithOutput(io.Discard))
+	h, err := habit.New("jog")
 	if err != nil {
 		t.Fatal(err)
 	}
-	h.Dates = []time.Time{
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1),
-	}
-	h.LogActivity()
-	want := 3
-	got := len(h.Dates)
+	h.Date = time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1)
+	h.Log()
+	want := 2
+	got := h.Streak
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
 }
 
-func TestHabit_PrintsMessageOnThreeDayStreak(t *testing.T) {
+func TestHabit_StartsNewActivityAfterBrokenStreak(t *testing.T) {
 	t.Parallel()
-	fakeTerminal := &bytes.Buffer{}
-	habitName := "jog"
-	h, err := habit.New(habitName, habit.WithOutput(io.Discard))
+	h, err := habit.New("jog")
 	if err != nil {
 		t.Fatal(err)
 	}
-	h.Output = fakeTerminal
-	h.Dates = []time.Time{
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1),
-	}
-	h.LogActivity()
-	want := fmt.Sprintf(messageOnContinousStreak, h.Name, 3)
-	got := fakeTerminal.String()
-	if want != got {
-		t.Errorf("want %s, got %s", want, got)
-	}
-}
+	// Setup date 5 days ago to simulate that the habit was logged 5 days ago.
+	h.Date = time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -5)
 
-func TestHabit_PrintsMessageOnStartingNewStreakAfterBreak(t *testing.T) {
-	t.Parallel()
-	fakeTerminal := &bytes.Buffer{}
-	habitName := "jog"
-	h, err := habit.New(habitName, habit.WithOutput(io.Discard))
-	if err != nil {
-		t.Fatal(err)
-	}
-	h.Output = fakeTerminal
-	h.Dates = []time.Time{
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -5),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -4),
-	}
-
-	h.LogActivity()
-
-	wantMessage := fmt.Sprintf(messageOnStaringNewStreakAfterBreak, h.Name, 4)
-	gotMessage := fakeTerminal.String()
-	if wantMessage != gotMessage {
-		t.Errorf("want %s, got %s", wantMessage, gotMessage)
-	}
-
-	wantDates := []time.Time{
-		time.Now().UTC().Truncate(24 * time.Hour),
-	}
-	gotDates := h.Dates
-
-	if !cmp.Equal(wantDates, gotDates) {
-		t.Error(cmp.Diff(wantDates, gotDates))
-	}
-}
-
-func TestHabit_PrintsNumberOfDaysOnNotBrokenCurrentStreak(t *testing.T) {
-	t.Parallel()
-	habitName := "jog"
-	h, err := habit.New(habitName, habit.WithOutput(io.Discard))
-	if err != nil {
-		t.Fatal(err)
-	}
-	h.Dates = []time.Time{
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -4),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -3),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1),
-	}
-	fakeTerminal := &bytes.Buffer{}
-	h.Output = fakeTerminal
+	got, _ := h.Check()
 	want := 5
-	h.Check()
-	wantMessage := fmt.Sprintf(messageOnNotBrokenStreakCheck, want, habitName)
-	gotMessage := fakeTerminal.String()
-	if wantMessage != gotMessage {
-		t.Errorf("want %s, got %s", wantMessage, gotMessage)
-	}
-
-}
-
-func TestHabit_PrintsNumberOfDaysSinceBrokenStreak(t *testing.T) {
-	t.Parallel()
-	habitName := "jog"
-	h, err := habit.New(habitName, habit.WithOutput(io.Discard))
-	if err != nil {
-		t.Fatal(err)
-	}
-	h.Dates = []time.Time{
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -4),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -3),
-	}
-	fakeTerminal := &bytes.Buffer{}
-	h.Output = fakeTerminal
-
-	want := 3
-	got := h.Check()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
 
-	wantMessage := fmt.Sprintf(messageOnBrokenStreakCheck, want, habitName)
-	gotMessage := fakeTerminal.String()
-	if wantMessage != gotMessage {
-		t.Errorf("want %s, got %s", wantMessage, gotMessage)
+	want = 1
+	got, _ = h.Log()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
 
-func TestHabit_LoadsDataFromJSONFile(t *testing.T) {
+func TestLoadHabitFromFile(t *testing.T) {
 	t.Parallel()
-	habitFilepath := "testdata/new_habit.json"
+	habitFilepath := "testdata/habit.json"
 	got, err := habit.LoadFromFile(habitFilepath)
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := habit.Habit{
-		Name:  "walk",
-		Dates: []time.Time{time.Date(2022, 07, 15, 00, 00, 00, 00, time.UTC)},
+	want := &habit.Habit{
+		Name:   "walk",
+		Date:   time.Date(2022, 07, 15, 00, 00, 00, 00, time.UTC),
+		Streak: 1,
 	}
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
 	}
 }
 
-func TestHabit_SavesHabitDataToFile(t *testing.T) {
+func TestSaveHabitToFile(t *testing.T) {
 	t.Parallel()
-	h, err := habit.New("run", habit.WithOutput(io.Discard))
+	h, err := habit.New("run")
 	if err != nil {
 		t.Fatal(err)
 	}
-	h.Dates = append(h.Dates,
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2),
-		time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1),
-	)
-	path := t.TempDir() + "/run.json"
+	h.Date = time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -1)
+
+	path := t.TempDir() + "/habit.json"
 	err = habit.SaveToFile(path, h)
 	if err != nil {
 		t.Fatal(err)
@@ -205,27 +132,27 @@ func TestHabit_SavesHabitDataToFile(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	h2.Output = io.Discard
+
 	if !cmp.Equal(h, h2) {
-		t.Error(cmp.Diff(h, h2))
+		t.Errorf("want %+v, got %+v", h, h2)
 	}
 }
 
 func TestHabit_SavesUpdatedHabitDataToFile(t *testing.T) {
 	t.Parallel()
-	habitFilepath := "testdata/new_habit.json"
-	h, err := habit.FromFile(habitFilepath, habit.WithOutput(io.Discard))
+	habitFilepath := "testdata/habit.json"
+	h, err := habit.LoadFromFile(habitFilepath)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	h.LogActivity()
+	h.Log()
 
 	path := t.TempDir() + "/path.json"
 
 	habit.SaveToFile(path, h)
 
-	h2, err := habit.FromFile(path, habit.WithOutput(io.Discard))
+	h2, err := habit.LoadFromFile(path)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -234,22 +161,14 @@ func TestHabit_SavesUpdatedHabitDataToFile(t *testing.T) {
 	}
 }
 
-var (
-	messageStartNewHabit                = "Good luck with your new habit '%s'! Don't forget to do it again tomorrow.\n"
-	messageOnContinousStreak            = "Nice work: you've done the habit '%s' for %d days in a row now. Keep it up!\n"
-	messageOnStaringNewStreakAfterBreak = "You last did the habit '%s' %d days ago, so you're starting a new streak today. Good luck!\n"
-	messageOnBrokenStreakCheck          = "It's been %d days since you did '%s'. It's okay, life happens. Get back on that horse today!\n"
-	messageOnNotBrokenStreakCheck       = "You're currently on a %d-day streak for '%s'. Stick to it!\n"
-)
-
-func TestMain(m *testing.M) {
-	testscript.RunMain(m, map[string]func() int{
-		"habit": func() int {
-			habit.RunCLI()
-			return 0
-		},
-	})
-}
-func TestScript(t *testing.T) {
-	testscript.Run(t, testscript.Params{Dir: "testdata/script"})
-}
+// func TestMain(m *testing.M) {
+// 	testscript.RunMain(m, map[string]func() int{
+// 		"habit": func() int {
+// 			habit.RunCLI()
+// 			return 0
+// 		},
+// 	})
+// }
+// func TestScript(t *testing.T) {
+// 	testscript.Run(t, testscript.Params{Dir: "testdata/script"})
+// }
